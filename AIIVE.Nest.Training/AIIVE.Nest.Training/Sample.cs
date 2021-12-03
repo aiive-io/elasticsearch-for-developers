558a0bb324f1b5c2cefe10add30312348afacbce
Carlos Machel <carlostiago@outlook.com.br>
2021-12-03 06:29:49 -0300

    adicionando projeto nest

---
+++ AIIVE.Nest.Training/AIIVE.Nest.Training/Sample.cs
@@ -0,0 +1,28 @@
+﻿using Nest;
+using System;
+
+namespace AIIVE.ES.NEST.Training
+{
+    public class Sample
+    {
+        public string Category { get; set; }
+
+        public string Currency { get; set; }
+
+        [PropertyName("customer_birth_date")]
+        public DateTime CustomerBirthDate { get; set; }
+
+        [PropertyName("customer_first_name")]
+        public string CustomerFirstName { get; set; }
+
+        public string CustomerFullName { get; set; }
+
+        public string CustomerGender { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public string CustomerLastName { get; set; }
+
+        public
+    }
+}
---
+++ AIIVE.Nest.Training/Aiive.Nest.Training.IntegrationTest/SamplesTests.cs
@@ -0,0 +1,77 @@
+using System;
+using Xunit;
+
+namespace Aiive.Nest.Training.IntegrationTest
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// 1. Quantos documentos esse índice tem?
+        /// </summary>
+        [Fact]
+        public void Teste1()
+        {
+
+        }
+
+        /// <summary>
+        /// 2. Como podemos criar um alias para esse índice?
+        /// </summary>
+        [Fact]
+        public void Teste2() { }
+
+        /// <summary>
+        /// 3. Como verificamos o mapeamento desse índice?
+        /// </summary>
+        [Fact]
+        public void Teste3() { }
+
+        /// <summary>
+        /// 4. Como verificamos o mapeamento de um campo desse índice específico?
+        /// </summary>
+        [Fact]
+        public void Teste4() { }
+
+        /// <summary>
+        /// 5.1 Primeiro precisamos mapear a propriedade "products" para ser do type "nested".
+        /// </summary>
+        [Fact]
+        public void Teste5_1() { }
+
+        /// <summary>
+        ///  5.2 Agora vamos transferir o índice ecommerce para o my-ecommerce.
+        /// </summary>
+        [Fact]
+        public void Teste5_2() { }
+
+        /// <summary>
+        ///    5.3 Vamos verificar como ficou o mapeamento do nosso my-ecommerce. Fazendo a nossa query de mapeamento vemos agora que produtos está com type nested.
+        /// </summary>
+        [Fact]
+        public void Teste5_3() { }
+
+        /// <summary>
+        /// 6. Qual a diferença na pesquisa?
+        /// </summary>
+        [Fact]
+        public void Teste6() { }
+
+        /// <summary>
+        /// 7. Nosso gerente de marketing perguntou se nós vendemos mais para pessoas que se identificam com o gênero Masculino ou Female.
+
+        /// </summary>
+        [Fact]
+        public void Teste7() { }
+
+        /// <summary>
+        /// 8. Agora ele perguntou pra gente quanto nós vendemos para cada gênero.
+        /// </summary>
+        [Fact]
+        public void Teste8()
+        {
+
+        }
+
+
+    }
+}
